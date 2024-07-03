cd3b30ca31f0fa773aeea68cfa71da7c50df3937
Samaid <samaid.saeed@arbisoft.com>
2024-07-03 21:02:48 +0500

    Added search input for sales section

--- src/components/home/auctions.tsx
+++ src/components/home/auctions.tsx
@@ -5,6 +5,9 @@ import { Button, ButtonGroup, Pagination, PaginationItem, PaginationLink } from 
 import { PaginationWrapper } from "./styles";
 import SortAndFilter from "./sortAndFilter";
 import { useState } from "react";
+
+const SCROLL_CONFIG = {scroll: false};
+
 export default function Auctions(props: any) {
 
   const router = useRouter();
@@ -13,24 +16,29 @@ export default function Auctions(props: any) {
   const sortBy = searchParams.get('sortBy');
   let page = Number(searchParams.get('page'));
   if (page === 0) page = 1;
+  const isDataAvailable = props.auctions.length > 0
 
   const handleNextPage = (nextPage: number) => {
     let newParams = `page=${nextPage}`;
     if(nextPage > 1 && searchParams.get('page')) {
       newParams = searchParams.toString().replace(`page=${page}`, `page=${nextPage}`)
     }
-    router.replace(`/?${newParams}`, {scroll: false});
+    router.replace(`/?${newParams}`, SCROLL_CONFIG);
   }
 
   const handleSort = (key: string) => {
-    router.replace(`/?page=${page}&sortBy=${key}&order=${sortOrder}`, {scroll: false});
+    router.replace(`/?page=${page}&sortBy=${key}&order=${sortOrder}`, SCROLL_CONFIG);
   }
 
   const onChangeOrder = (value: string) => {
     if(sortBy) {
-      router.replace(`/?page=${page}&sortBy=${sortBy}&order=${value}`, {scroll: false});
+      router.replace(`/?page=${page}&sortBy=${sortBy}&order=${value}`, SCROLL_CONFIG);
     }
     setSortOrder(value)
+  }
+
+  const handleSearch = (keyword: string) => {
+    router.replace(`/?filter=${keyword}`, SCROLL_CONFIG);
   }
 
   return <>
@@ -40,27 +48,30 @@ export default function Auctions(props: any) {
         sortBy={sortBy} 
         sortOrder={sortOrder} 
         handleSort={handleSort} 
-        setSortOrder={onChangeOrder} />
+        setSortOrder={onChangeOrder}
+        handleSearch={handleSearch} />
     </div>
     <div className="col-9 col-md-9">
     {
-        props.auctions.map((auction: any) => (
+        isDataAvailable ? props.auctions.map((auction: any) => (
           <Auction auction={auction} key={auction.ref}/>
-        ))
+        )) : <p>No Sales Available</p>
       }
-    <PaginationWrapper>
-    <Pagination className="mt-3">
-      {
-        [1,2,3,4,5].map((num: number) => (
-          <PaginationItem key={num} active={num === page}>
-            <PaginationLink onClick={() => handleNextPage(num)}>
-              {num}
-            </PaginationLink>
-          </PaginationItem>
-        ))
-      }
-    </Pagination>
-    </PaginationWrapper>
+    {
+      isDataAvailable && <PaginationWrapper>
+      <Pagination className="mt-3">
+        {
+          [1,2,3,4,5].map((num: number) => (
+            <PaginationItem key={num} active={num === page}>
+              <PaginationLink onClick={() => handleNextPage(num)}>
+                {num}
+              </PaginationLink>
+            </PaginationItem>
+          ))
+        }
+      </Pagination>
+      </PaginationWrapper>
+    }
     </div>
   </div>
   </>
